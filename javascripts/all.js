4dbd8dc42003ad6f74ecc5868fa9f22eaa6fb26f
tetsuroshimura <tetsuro@uniba.jp>
2016-10-24 01:41:20 +0900

    Automated commit at 2016-10-23 16:41:20 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,6 +1,18 @@
 $(function(){
   var $video = $('video');
-  var $body = $('#body');
+  var $body = $('body');
+  var $main = $('#main');
+
+  // modify text "Loading..."
+  var dot = 0;
+  var loadingText = "Loading.";
+  var loadingTextFunc = setInterval(function() {
+    if (loadingText == "Loading..."){
+      loadingText = "Loading";
+    }
+    loadingText = loadingText + ".";
+    $body.attr('data-loading', loadingText);
+  }, 400);
 
   // Show contents when vedeo can be played
   var count = 0;
@@ -8,9 +20,17 @@ $(function(){
     $video[i].addEventListener('canplaythrough', function() {
       count++;
       if ( count == $video.length ) {
-        $body.css('opacity', 1);
+        $body.removeClass('loading');
+        $main.css('opacity', 1);
+        clearInterval(loadingTextFunc);
       }
     }, false);
   }
+  // Show contents after 3s count
+  setTimeout(function(){
+    $body.removeClass('loading');
+    $main.css('opacity', 1);
+    clearInterval(loadingTextFunc);
+  }, 3000);
 
 });
