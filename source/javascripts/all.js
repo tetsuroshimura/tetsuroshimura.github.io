774ba5ba87a033333948d1dfaea5240c60966393
saoorim <saori.miura.sm@gmail.com>
2016-10-24 14:35:23 +0900

    INDEX: スマホ（iPad含む）では .bgimg の要素を削除、ローディングも表示させない

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,34 +3,52 @@ $(function(){
   var $body = $('body');
   var $main = $('#main');
 
-  // modify text "Loading..."
-  var dot = 0;
-  var loadingText = "Loading.";
-  var loadingTextFunc = setInterval(function() {
-    if (loadingText == "Loading..."){
-      loadingText = "Loading";
-    }
-    loadingText = loadingText + ".";
-    $body.attr('data-loading', loadingText);
-  }, 400);
-
-  // Show contents when vedeo can be played
-  var count = 0;
-  for (var i = 0; i < $video.length; i++) {
-    $video[i].addEventListener('canplaythrough', function() {
-      count++;
-      if ( count == $video.length ) {
-        $body.removeClass('loading');
-        $main.css('opacity', 1);
-        clearInterval(loadingTextFunc);
-      }
-    }, false);
+  // User Agent
+  var agent = navigator.userAgent;
+  if ( agent.search("iPad") != -1 || agent.search("iPhone") != -1 || agent.search("Android") != -1 ){
+    var isMobile = true;
   }
-  // Show contents after 3s count
-  setTimeout(function(){
+
+
+  if ( isMobile ) {
     $body.removeClass('loading');
     $main.css('opacity', 1);
-    clearInterval(loadingTextFunc);
-  }, 3000);
+    var bgImg = $('.bgimg');
+    for (var i = 0; i < bgImg.length; i++) {
+      bgImg[i].remove();
+    }
+
+  } else {
+    // modify text "Loading..."
+    var dot = 0;
+    var loadingText = "Loading.";
+    var loadingTextFunc = setInterval(function() {
+      if (loadingText == "Loading..."){
+        loadingText = "Loading";
+      }
+      loadingText = loadingText + ".";
+      $body.attr('data-loading', loadingText);
+    }, 400);
+
+    // Show contents when vedeo can be played
+    var count = 0;
+    for (var i = 0; i < $video.length; i++) {
+      $video[i].addEventListener('canplaythrough', function() {
+        count++;
+        if ( count == $video.length ) {
+          $body.removeClass('loading');
+          $main.css('opacity', 1);
+          clearInterval(loadingTextFunc);
+        }
+      }, false);
+    }
+    // Show contents after 3s count
+    setTimeout(function(){
+      $body.removeClass('loading');
+      $main.css('opacity', 1);
+      clearInterval(loadingTextFunc);
+    }, 3000);
+
+  }
 
 });
